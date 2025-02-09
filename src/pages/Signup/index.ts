76f958bc022185de579f3767e2e189ab33acfb8f
Ricardo Fuzeto <ricardofuzeto@gmail.com>
2025-02-09 13:24:45 -0300

    Added signup page same as login

--- src/pages/Signup/index.ts
+++ src/pages/Signup/index.ts
@@ -1,9 +1,47 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatCard, MatCardContent } from '@angular/material/card';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInput, MatLabel } from '@angular/material/input';
 
 @Component({
   selector: '',
   template: `
-    <p>Signup</p>
+    <mat-card appearance="outlined">
+      <mat-card-content>
+        <form id="login-form" (submit)="onSubmit($event)">
+          <mat-form-field>
+            <mat-label>Username:</mat-label>
+            <input matInput type="text" name="username" />
+          </mat-form-field>
+          <mat-form-field>
+            <mat-label>Password:</mat-label>
+            <input matInput type="password" name="password" />
+          </mat-form-field>
+          <mat-form-field>
+            <mat-label>Confirm password:</mat-label>
+            <input matInput type="password" name="passwordConfirm" />
+          </mat-form-field>
+          <button mat-flat-button type="submit">Create account</button>
+        </form>
+      </mat-card-content>
+    </mat-card>
   `,
+  imports: [
+    MatButton,
+    MatCard,
+    MatCardContent,
+    MatFormField,
+    MatInput,
+    MatLabel,
+  ],
+  styleUrl: './index.scss',
 })
-export class SignupPage {}
+export class SignupPage {
+  onSubmit = (event: SubmitEvent) => {
+    event.preventDefault();
+    const data = new FormData(event.target as HTMLFormElement);
+    console.log(data.get('username'));
+    console.log(data.get('password'));
+  };
+}
